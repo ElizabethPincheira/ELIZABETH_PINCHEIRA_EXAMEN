8378312603b19124445abf8f9621a9c7cde7189a
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2024-11-23 03:14:14 -0300

    html y css de ejercicio1 completos

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
+#index.html--------------------------------------
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+
+#ejercicio1.html----------------------------------
+@app.route('/ejercicio1')
+def ejercicio1():
+    return render_template('ejercicio1.html')
+
+
 if __name__ == '__main__':
     app.run()
+
+
